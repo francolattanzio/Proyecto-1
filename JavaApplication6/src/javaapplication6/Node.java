833a67bd8e054edf6714bffa69645272de00a484
Franco <franco.lattanzio@correo.unimet.edu.ve>
2023-10-14 08:02:14 -0400

    class Node

--- JavaApplication6/src/javaapplication6/Node.java
+++ JavaApplication6/src/javaapplication6/Node.java
@@ -7,8 +7,55 @@ package javaapplication6;
 
 /**
  *
- * @author user
+ * @author aver q pasa
  */
-public class Node {
-    
+public class Node<T> {
+
+    private T data;
+    private Node next;
+
+    /**
+     *
+     * @param datum
+     */
+    public Node(T datum) {
+        this.data = datum;
+        this.next = null;
+    }
+
+    /**
+     * Get Node's data
+     *
+     * @return the data inside Node
+     */
+    public T getData() {
+        return data;
+    }
+
+    /**
+     * Sets new data to Node
+     *
+     * @param data
+     */
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    /**
+     *
+     * @return a reference to the next Node
+     */
+    public Node getNext() {
+        return next;
+    }
+
+    /**
+     * Sets new Node to reference to
+     *
+     * @param next
+     */
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }
